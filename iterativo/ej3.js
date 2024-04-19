7b94a958022bf47f2d87c5f187a65049c308eb45
Catalinagonzalez99 <164504416+Catalinagonzalez99@users.noreply.github.com>
2024-04-19 11:18:48 -0300

    Add files via upload

--- iterativo/ej3.js
+++ iterativo/ej3.js
@@ -1,13 +1,17 @@
 /*sume 3 números ingresados por consola donde por cada número ingresado debe mostrar el valor del resultado actual o sea en cada iteración*/
 const leer = require("prompt-sync")();
-const RESPUESTA_TOTAL = ("resultado")   
+const RESPUESTA_TOTAL = ("resultado") ;
 console.log("ingresar numero A"); 
-let numeroIngresadoA = Number(leer());
-
-
+ numeroIngresadoA = Number(leer());
 console.log("ingresar numero B"); 
  numeroIngresadoB = Number(leer());
- 
- let resultadoFinalA = numeroIngresadoA + numeroIngresadoB
- console.log ("suma de numeros ingresados");
+ console.log("ingresar numero C"); 
+ numeroIngresadoC = Number(leer());
+ let suma = 0;
+ console.log("suma de numeros ingresados",RESPUESTA_TOTAL, numeroIngresadoA+numeroIngresadoB );
+
+
+for (let i= 1;  i <= 3;i++){
+    suma +=i 
+}
 
--- iterativo/ej5.js
+++ iterativo/ej5.js
@@ -2,3 +2,17 @@
 const leer = require("prompt-sync")();
 console.log("ingresar numero "); 
 let numeroIngresado = Number(leer());
+const RESPUESTA_TOTAL = ("resultado") ;
+console.log("ingresar numero A"); 
+ numeroIngresadoA = Number(leer());
+ console.log("ingresar numero B"); 
+ numeroIngresadoB = Number(leer());
+ console.log("ingresar numero C"); 
+ numeroIngresadoC = Number(leer());
+ let suma = 0;
+ console.log("suma de numeros ingresados",RESPUESTA_TOTAL, numeroIngresadoA+numeroIngresadoB );
+
+
+for (let i= 1;  i <= 3;i++){
+    suma += i
+}
